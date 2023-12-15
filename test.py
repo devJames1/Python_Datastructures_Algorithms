06528733eb15e6040ea6ba82f66e4356118610a2
James Okolie <zebedee788@gmail.com>
2023-12-15 10:55:58 +0100

    queue overall understanding completed

--- test.py
+++ test.py
@@ -185,4 +185,349 @@ from collections import deque
 # history.popleft()
 # print(history)
 
+###################################
+# Queue Implementation using list
+###################################
+
+# queue = []
+# queue.append("a")
+# queue.append("b")
+# queue.append("c")
+# print("Initial queue")
+# print(queue)
+# print("\nElements dequeued from queue")
+# print(queue.pop(0))
+# print(queue.pop(0))
+# print(queue.pop(0))
+# print("\nQueue after removing elements")
+# print(queue)
+
+
+
+###################################################
+# Implementation using queue.Queue
+###################################################
+# from queue import Queue
+
+# q = Queue(maxsize = 3)
+# print(q.qsize())
+# q.put("a")
+# q.put("b")
+# q.put("c")
+# print("\nFull: ", q.full())
+# print("\nElements dequeued from the queue")
+# print(q.get())
+# print(q.get())
+# print(q.get())
+# print("\nEmpty: ", q.empty())
+# q.put(1)
+# print("\nEmpty: ", q.empty())
+# print("\nFull: ", q.full())
+# print(q.qsize())
+
+
+# Implementing custom Queue class
+
+from collections import deque
+
+# class Queue:
+
+#     def __init__(self):
+#         self._elements = deque()
+    
+#     def enqueue(self, element):
+#         self._elements.append(element)
+    
+#     def dequeue(self):
+#         return self._elements.popleft()
+
+
+# fifo = Queue()
+# fifo.enqueue("1st")
+# fifo.enqueue("2nd")
+# fifo.enqueue("3rd")
+
+# print(fifo.dequeue())
+# print(fifo.dequeue())
+# print(fifo.dequeue())
+
+
+#  If you want, you may improve your class by making it iterable
+#  and able to report its length and optionally accept initial elements: 
+# 
+# 
+class iterableMixin:
+    def __len__(self):
+        return len(self._elements)
+    
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+class Queue(iterableMixin):
+
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+    
+    def __len__(self):
+        return len(self._elements)
+    
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+    
+    def dequeue(self):
+        return self._elements.popleft()
+    
+
+# fifo = Queue("1st", "2nd", "3rd")
+# print(len(fifo))
+
+# for element in fifo:
+#     print(element)
+
+# print(len(fifo))
+
+
+# We can extend your Queue class using inheritance and override the 
+# dequeue() method to remove elements from the top of the stack:
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
+
+# lifo = Stack()
+# lifo.enqueue("1st")
+# lifo.enqueue("2nd")
+# lifo.enqueue("3rd")
+
+# print(lifo.dequeue())
+# print(lifo.dequeue())
+# print(lifo.dequeue())
+
+
+
+# When you push a new element onto a non-empty heap, 
+# it’ll end up in the right spot, maintaining the heap 
+# invariant. Note that this doesn’t necessarily mean that 
+# the resulting elements will become sorted
+from heapq import heappush
+from heapq import heappop
+
+# fruits = []
+# heappush(fruits, "orange")
+# heappush(fruits, "banana")
+# heappush(fruits, "apples")
+# heappush(fruits, "ap")
+# heappush(fruits, "app")
+
+# print(fruits)  # ['ap', 'app', 'banana', 'orange', 'apples']
+# The point of a heap isn’t so much about sorting elements
+#  but rather keeping them in a certain relationship to allow
+#  for quick lookup. What really matters is that the first
+#  element on a heap always has the smallest (min-heap) or
+#  the highest (max-heap) value, depending on how you define
+#  the condition for the mentioned relationship. Python’s
+#  heaps are min-heaps, which means that the first element
+#  has the smallest value.
+
+
+# When you pop an element from a heap, you’ll always get
+#  the first one, while the remaining elements might
+#  shuffle a little bit:
+
+# print(heappop(fruits))
+# print(fruits)
+
+
+# Now, how do you throw priorities into the mix? The heap compares
+# elements by value rather than by their priority, after all. 
+# To work around this, you can leverage Python’s tuple comparison, 
+# which takes into account the tuple’s components, looking from 
+# left to right until the outcome is known:
+
+# person1 = ("John", "Brown", 42)
+# person2 = ("John", "Doe", 42)
+# person3 = ("John", "Doe", 24)
+
+# print( person1 < person2)
+# # True
+# print( person2 < person3)
+# # False
+
+
+##########################################
+# Building a Priority Queue Data Type
+##########################################
+from collections import deque
+from heapq import heappush, heappop
+
+# class PriorityQueue:
+#     def __init__(self):
+#         self._elements = []
+
+#     def enqueue_with_priority(self, priority, value):
+#         heappush(self._elements, (priority, value))
+
+#     def dequeue(self):
+#         print(self._elements)
+#         return heappop(self._elements)
+    
+
+# Unfortunately, there are a few problems with the above 
+# implementation that become apparent when you try to use it:
+
+# CRITICAL = 3
+# IMPORTANT = 2
+# NEUTRAL = 1
+
+# messages = PriorityQueue()
+# messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on") 
+# messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+# messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+# messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+# print(messages.dequeue())
+
+# Ultimately, it’s up to you how you want to define the order 
+# of your priorities. In this tutorial, a lower priority 
+# corresponds to a lower numeric value, while a higher priority has a greater value.
+
+# Because Python’s heap is a min-heap, its first element always 
+# has the lowest value. To fix this, you can flip the sign of a 
+# priority when pushing a tuple onto the heap, making the 
+# priority a negative number so that the highest one becomes 
+# the lowest:
+
+# class PriorityQueue:
+#     def __init__(self):
+#         self._elements = []
+
+#     def enqueue_with_priority(self, priority, value):
+#         heappush(self._elements, (-priority, value))
+
+#     def dequeue(self):
+#         print(self._elements)
+#         return heappop(self._elements)[1]
+  
+    
+# CRITICAL = 3
+# IMPORTANT = 2
+# NEUTRAL = 1
+
+# messages = PriorityQueue()
+# messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on") 
+# messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+# messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+# messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+# print(messages.dequeue())
+
+
+# However, there are two problems with your implementation. 
+# One of them you can already see in the output, while the 
+# other will only manifest itself under specific circumstances. 
+# Can you spot these problems?
+
+
+# Handling Corner Cases in Your Priority Queue
+
+# To be clear, this is a direct consequence of tuple comparison
+# in Python, which moves to the next component in a tuple if 
+# the earlier ones didn’t resolve the comparison. So, if two 
+# messages have equal priorities, then Python will compare 
+# them by value, which would be a string in your example. 
+# Strings follow the lexicographic order, in which the word 
+# Hazard comes before the word Windshield, hence the 
+# inconsistent order.
+
+# Consider the following data class to represent messages
+#  in your queue:
+from dataclasses import dataclass
+
+# @dataclass(order=True)
+# class Message:
+#     event: str
+
+# wipers = Message("Windshield wipers turned on")
+# hazard_lights = Message("Hazard lights turned on")
+# abs_warn = Message("ABS engaged")
+
+# print(wipers < hazard_lights)
+
+# For example, when you enqueue a critical message and an 
+# important message, Python determines their order 
+# unambiguously by looking at the corresponding priorities. 
+# However, as soon as you try enqueuing another critical 
+# message, you’ll get a familiar error
+
+# messages.enqueue_with_priority(CRITICAL, wipers)
+# messages.enqueue_with_priority(IMPORTANT, hazard_lights) #[(-3, Message(event='Windshield wipers turned on')), (-2, Message(event='Hazard lights turned on'))]
+# print(messages.dequeue()) 
+
+# messages.enqueue_with_priority(CRITICAL, abs_warn) #Traceback (most recent call last):TypeError: '<' not supported between instances of 'Message' and 'Message'
+
+# print(messages.dequeue()) 
+
+# This time around, the comparison fails because two of the messages 
+# are of equal priority and Python falls back to comparing them 
+# by value, which you haven’t defined for your custom Message 
+# class instances.
+# Ths was solved by adding a keyword argument to the @dataclass(order=True) decorator like so.
+
+# Now it can compare data objects but still doesn't sort by insertion rather than hexicographically.
+# Here's another FIX that handles both situations.
+
+# You can solve both problems—that is, the sort instability and 
+# the broken tuple comparison—by introducing another component 
+# to the elements stored on the heap. This extra component 
+# should be comparable and represent the time of arrival. 
+# When placed between the element’s priority and value in 
+# a tuple, it’ll resolve the order if two elements have the 
+# same priority, regardless of their values.
+
+# You can use the count() iterator from the itertools module to 
+# count from zero to infinity in a concise way:
+
+from itertools import count
+
+# Refactoring the Code Using a Mixin Class
+
+
+class PriorityQueue(iterableMixin):
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
+    
+
+
+# CRITICAL = 3
+# IMPORTANT = 2
+# NEUTRAL = 1
+
+# messages = PriorityQueue()
+# messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on") 
+# messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+# messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+# messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+# print(len(messages))
+# for objec in messages:
+#     print(objec)
+
+
+##################################
+# Using Queues in Practice
+###################################
+
 
